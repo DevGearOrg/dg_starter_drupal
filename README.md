233dcffa86852a178035d3fffe88046e8223a785
Maksym Mospanenko <mmospanenko@gmail.com>
2018-11-09 08:22:06 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# dg_starter_drupal
+# DevGear Drupal Starter
+
 DevGear Starter Kit for Drupal projects with Docker.
+
+## Prerequirements:
+
+We're using some popular tools to help acheave your goals:
+
+1. Install Docker for your operational system: [Docker for Developers](https://www.docker.com/get-started)
+2. Make sure you have working GNU Make or use makefile as "cheatsheet"
+3. You should use Git to clone this StarterKit
+
+## Quick start:
+
+1. Create new project based on current file structure and content
+2. Step into this folder in termial and execite `make run` command
+3. Check result in your web browser following http://localhost
+
+## Makefile aliases reference:
+
+1. `make run` - wrapper around `docker-compose up`. Prepares and starts necwssary containt
+2. `make stop` - suspends all working containers from this app
+3. `make clear` - stops and removes all containers and data images from this project
---
+++ public_html/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DevGear Starter</title>
+</head>
+
+<body>
+    <h1>It Works!</h1>
+    <p>Put your files into publick_html folder (replace index.php file).</p>
+    Navigate to <a href="http://localhost:8080">PhpMyAdmin</a> for database management.
+    <h3>Database credentials:</h3>
+    <table>
+        <tr>
+            <td>DB_HOST:</td>
+            <td>mysql</td>
+        </tr>
+        <tr>
+            <td>DB_NAME:</td>
+            <td>drupal</td>
+        </tr>
+        <tr>
+            <td>DB_USER:</td>
+            <td>drupal</td>
+        </tr>
+        <tr>
+            <td>DB_PASSWORD:</td>
+            <td>drupal</td>
+        </tr>
+    </table>
+
+    <p>See docker-compose.yml as reference.</p>
+</body>
+
+
+</html>
